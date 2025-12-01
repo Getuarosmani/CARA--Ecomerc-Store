b5ea9a4101e211a9811d88c2dcbd5b33aa0e0486
getuar <getuarosmani91@gmail.com>
2025-12-01 12:51:16 +0100

    Fix routing for GitHub Pages using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './Context/CartContex'
-import { Home } from "./pages/Home/Home"
-import { Shop } from "./pages/Shop/Shop"
-import { Blog } from "./pages/Blog/Blog"
-import { About } from "./pages/About/About"
-import { Contact } from "./pages/Contact/Contact"
-import { Cart } from "./pages/Cart/Cart"
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './Context/CartContex';
+import { Home } from "./pages/Home/Home";
+import { Shop } from "./pages/Shop/Shop";
+import { Blog } from "./pages/Blog/Blog";
+import { About } from "./pages/About/About";
+import { Contact } from "./pages/Contact/Contact";
+import { Cart } from "./pages/Cart/Cart";
 import { ProductDetalis } from './pages/Product-Detalis/ProductDetalis';
 
 function App() {
   return (
     <CartProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Shop" element={<Shop />} />
@@ -21,9 +21,9 @@ function App() {
           <Route path="/Cart" element={<Cart />} />
           <Route path="/product/:id" element={<ProductDetalis />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </CartProvider>
   )
 }
 
-export default App
+export default App;
